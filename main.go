0605961b42e2b50401090acc953e9032baf1f066
RosalieBdm <91187050+RosalieBdm@users.noreply.github.com>
2021-10-14 13:13:04 +0200

    Update main.go
    Ajout struct utilisateur, music et methodes vides

--- main.go
+++ main.go
@@ -1,9 +1,38 @@
 import {
-  fmt
+  "fmt"
+  "container/list"
 }
 
-func recherche_musique ( param ... interface{}) {
-  if param == nil {  # pas de filtre
+type Utilisateur struct(){
+  num_utilisateur int 
+  list_music list
+  genre_fav string 
+}
+
+type Music struct() {
+  titre string 
+  album string
+  artiste string
+  genre string
+}
+
+func find_genre_fav (Utilisateur a) (Music) {
+  
+}
+
+
+
+func find_musique ( param ... string, int distance ) (Musique){
+  // distance est demandé à l'utilisateur et permet de savoir si il veut découvrir une musique donc on recherche une musique plus loin ou si il veut une musique qu'il est sûr d'aimer
+
+  if param == nil {  //pas de filtre donc on parcourt tout 
   }
   
-  if param[0] == 
+  if param == "genre" {
+  }
+  
+  if param == "artiste" {
+  }
+  
+  if param == "album" {
+  }
